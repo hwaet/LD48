65ebd59f86691a6661be6a7a2cb2caf74d2bfe78
greg <greg@hwaetgames.com>
2021-04-24 18:07:00 -0400

    victory condition! recipe checks and such

--- Assets/Scripts/DeliveryBehavior.cs
+++ Assets/Scripts/DeliveryBehavior.cs
@@ -1,30 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DeliveryBehavior : MonoBehaviour
 {
-
+    SceneWrangler sceneWrangler;
+    public List<Recipe> orderList;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        reloadOrderList();
+    }
+
+    private void Update()
+    {
+        checkForVictory();
+    }
+
+    [ContextMenu("reload orders")]
+    private void reloadOrderList()
+    {
+        sceneWrangler = FindObjectOfType<SceneWrangler>();
+        StageSettings_ld48 settings = (StageSettings_ld48)sceneWrangler.currentSceneContainer.stageSettings;
+        orderList.Clear();
+        foreach (Recipe recipe in settings.recipesOrders)
+        {
+            orderList.Add(recipe);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public void checkOrder(PlateBehavior deliveredPlate)
     {
-        
+        compareAgainstOrder(deliveredPlate.contents);
+        Debug.Log("Delivered this plate:" + deliveredPlate.name);
+    }
+
+    public void checkOrder(FoodBehavior deliveredFood)
+    {
+        List<FoodBehavior> tempFoodPlate = new List<FoodBehavior>();
+        tempFoodPlate.Add(deliveredFood);
+        compareAgainstOrder(tempFoodPlate);
+        Debug.Log("Delivered this food:" + deliveredFood.name);
     }
 
     [ContextMenu("check order")]
-    public void checkOrder(FoodBehavior deliveredFood)
+    public void compareAgainstOrder(List<FoodBehavior> deliveredFood)
     {
-        Debug.Log("Delivered this food:" + deliveredFood.name);
+        foreach (Recipe recipe in orderList)
+        {
+            bool successfullDelivery = recipe.compareAgainstFood(deliveredFood);
+
+            if (successfullDelivery == true)
+            {
+                Debug.Log("You have successfully filled an order:" + recipe.recipeName);
+                orderList.Remove(recipe);
+                
+                foreach (FoodBehavior food in deliveredFood)
+                {
+                    GameObject.Destroy(food.gameObject);
+                }
+                return;
+                
+            }
+        }
+        
     }
 
-    public void checkOrder(PlateBehavior deliveredFood) {
-        Debug.Log("Delivered this food:" + deliveredFood.name);
+    [ContextMenu("check for victory condition")]
+    public void checkForVictory()
+    {
+        if (orderList.Count == 0)
+        {
+            Debug.Log("You win this stage!!!!!");
+            sceneWrangler.loadTargetContainer();
+        }
     }
 }
--- Assets/Scripts/ScriptableObjects/Recipe.cs
+++ Assets/Scripts/ScriptableObjects/Recipe.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 
 [CreateAssetMenu]
@@ -8,16 +9,64 @@ public class Recipe : ScriptableObject
     public string recipeName;
     public Texture2D icon;
     public List<RecipeComponent> FoodItems;
+    public List<string> foodTypeList;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnValidate()
     {
-        
+        foodTypeList.Clear();
+        foreach (RecipeComponent food in FoodItems)
+        {
+            foodTypeList.Add(food.foodName);
+        }
+        foodTypeList.Sort();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool compareAgainstFood(List<FoodBehavior> deliveredFood)
     {
-        
+        //check size of food list
+        if (deliveredFood.Count != FoodItems.Count) return false;
+
+        //check names of foods
+        List<string> foodTypes = new List<string>();
+        foreach (FoodBehavior food in deliveredFood)
+        {
+            foodTypes.Add(food.FoodType);
+        }
+        foodTypes.Sort();
+        if (areListsEqual(foodTypeList,foodTypes) == false)
+        {
+            Debug.Log(this.recipeName + "Name mismatch on food");
+            return false; 
+        }
+
+        //checks donenes
+        foreach (FoodBehavior food in deliveredFood)
+        {
+            if (food.doneness != FoodBehavior.Doneness.Cooked)
+            {
+                Debug.Log(this.recipeName + "Food is over/under done");
+                return false;
+            }
+        }
+
+        //checks breadiness
+        //TODO
+
+        //checks stuffing
+        //TODO
+
+        return true;
+    }
+
+    bool areListsEqual(List<string> lista, List<string> listb)
+    {
+        if (lista.Count != listb.Count) return false;
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            if (lista[i] != listb[i]) return false;
+        }
+
+        return true;
     }
 }
