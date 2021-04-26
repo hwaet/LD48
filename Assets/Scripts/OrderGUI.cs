adb5d2035e816d08a579f364fffe273f2e37b560
greg <greg@hwaetgames.com>
2021-04-25 22:28:57 -0400

    Gui Manager and prefab for order display

---
+++ Assets/Scripts/OrderGUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OrderGUI : MonoBehaviour
+{
+    public Image RecipeIcon;
+    public Image BreadingIcon1;
+    public Image BreadingIcon2;
+
+}
